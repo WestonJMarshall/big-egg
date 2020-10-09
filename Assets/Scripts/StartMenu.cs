98f2ba8ad4effbac7454e6e9b473d397bf85fd5a
bhx4874 <bhx4874@g.rit.edu>
2020-10-09 13:08:51 -0400

    Start Menu
    Added the start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartMenu : MonoBehaviour
+{
+    public GameObject startUI;
+    public GameObject helpUI;
+
+    public void Awake()
+    {
+
+    }
+    public void StartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Help()
+    {
+        startUI.SetActive(false);
+        helpUI.SetActive(true);
+    }
+
+    public void BackToStart()
+    {
+        startUI.SetActive(true);
+        helpUI.SetActive(false);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
+
